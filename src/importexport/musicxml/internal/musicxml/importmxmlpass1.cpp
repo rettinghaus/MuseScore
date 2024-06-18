89b0828f1009f002c4c18a6086c9dc506cf53085
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-06-18 15:41:19 +0200

    export correct staff-distance

--- src/importexport/musicxml/internal/musicxml/importmxmlpass1.cpp
+++ src/importexport/musicxml/internal/musicxml/importmxmlpass1.cpp
@@ -2595,6 +2595,7 @@ void MusicXMLParserPass1::measure(const String& partId,
                     skipLogCurrElem();
                 }
             }
+            m_e.skipCurrentElement();        // skip but don't log
         } else if (m_e.name() == "sound") {
             m_e.skipCurrentElement();        // skip but don't log
         } else {
