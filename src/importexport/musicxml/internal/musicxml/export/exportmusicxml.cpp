a43ec16359915757eb43fffe8d79ef9b2525cc9a
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-07-18 19:01:09 +0200

    import/export hBox

--- src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
+++ src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
@@ -8252,6 +8252,8 @@ void ExportMusicXml::writeMeasure(const Measure* const m,
     if (isFirstActualMeasure) {
         writeStaffDetails(m_xml, part);
         writeInstrumentDetails(part->instrument(), m_score->style().styleB(Sid::concertPitch));
+    } else if (mpc.systemStart && !part->show()) {
+        writeStaffDetails(m_xml, part);
     }
 
     // output attribute at start of measure: measure-style
