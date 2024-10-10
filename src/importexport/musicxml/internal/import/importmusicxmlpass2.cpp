aa78228f7a9414e9670f59db5f04548cbe195704
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-10-10 12:29:04 +0200

    set position

--- src/importexport/musicxml/internal/import/importmusicxmlpass2.cpp
+++ src/importexport/musicxml/internal/import/importmusicxmlpass2.cpp
@@ -7249,8 +7249,10 @@ Note* MusicXmlParserPass2::note(const String& partId,
         }
 
         if (dotPosition == "above") {
+            note->setDotPosition(DirectionV::UP);
             note->setUserDotPosition(DirectionV::UP);
         } else if (dotPosition == "below") {
+            note->setDotPosition(DirectionV::DOWN);
             note->setUserDotPosition(DirectionV::DOWN);
         } else {
             note->setUserDotPosition(DirectionV::AUTO);
