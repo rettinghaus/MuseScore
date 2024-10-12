76af7797f6bc868529fd8d3e0cc8f55a728d018c
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-10-12 16:30:29 +0200

    add extra check

--- src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
+++ src/importexport/musicxml/internal/musicxml/export/exportmusicxml.cpp
@@ -7389,7 +7389,7 @@ void ExportMusicXml::print(const Measure* const m, const int partNr, const int f
             }
 
             m_xml.endElement();
-        } else {
+        } else if (attributes) {
             m_xml.tag("print", attributes);
         }
     } else if (m->prev() && m->prev()->isHBox()) {
