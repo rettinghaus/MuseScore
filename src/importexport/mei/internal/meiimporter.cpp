d585967e66a417399c30f7eb50724a1992ab21b3
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-11-18 20:29:38 +0100

    set text

--- src/importexport/mei/internal/meiimporter.cpp
+++ src/importexport/mei/internal/meiimporter.cpp
@@ -2561,6 +2561,7 @@ bool MeiImporter::readGliss(pugi::xml_node glissNode, Measure* measure)
     gliss->setStartElement(startNote);
     gliss->setTrack(startNote->track());
     gliss->setParent(startNote);
+    gliss->setText(glissNode.text());
 
     m_score->addElement(gliss);
 
