cd371555fc982e6b1bb48a6e2b8ab9740d313311
Klaus Rettinghaus <klaus.rettinghaus@gmail.com>
2024-12-04 19:08:50 +0100

    improve import and export

--- src/importexport/mei/internal/meiimporter.cpp
+++ src/importexport/mei/internal/meiimporter.cpp
@@ -2644,7 +2644,10 @@ bool MeiImporter::readInstrDef(pugi::xml_node instrDefNode, Part* part)
     libmei::InstrDef meiInstrDef;
     meiInstrDef.Read(instrDefNode);
 
+    const int port = -1;
+
     part->setMidiProgram(meiInstrDef.GetMidiInstrnum());
+    part->setMidiChannel(meiInstrDef.GetMidiChannel(), port);
 
     return true;
 }
