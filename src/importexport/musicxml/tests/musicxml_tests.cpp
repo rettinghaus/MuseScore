c6cb9619ecc2e3c7951607809bb1479b7d03b266
rettinghaus <klaus.rettinghaus@gmail.com>
2025-01-18 11:45:19 +0100

    add test

--- src/importexport/musicxml/tests/musicxml_tests.cpp
+++ src/importexport/musicxml/tests/musicxml_tests.cpp
@@ -1072,6 +1072,9 @@ TEST_F(MusicXml_Tests, partNames) {
 TEST_F(MusicXml_Tests, partNames2) {
     musicXmlIoTest("testPartNames2");
 }
+TEST_F(MusicXml_Tests, partSymbol) {
+    musicXmlImportTestRef("testPartSymbol");
+}
 TEST_F(MusicXml_Tests, pedalChanges) {
     musicXmlIoTest("testPedalChanges");
 }
