bb3adeca6ddca3431d166d4efb92dfe3d1033364
rettinghaus <klaus.rettinghaus@gmail.com>
2025-03-26 14:03:54 +0100

    add test

--- src/importexport/musicxml/tests/musicxml_tests.cpp
+++ src/importexport/musicxml/tests/musicxml_tests.cpp
@@ -1048,6 +1048,9 @@ TEST_F(MusicXml_Tests, numerals) {
 TEST_F(MusicXml_Tests, ornaments) {
     musicXmlIoTest("testOrnaments");
 }
+TEST_F(MusicXml_Tests, ornamentAccids) {
+    musicXmlIoTest("testOrnamentAccids");
+}
 TEST_F(MusicXml_Tests, overlappingSpanners) {
     musicXmlIoTest("testOverlappingSpanners");
 }
