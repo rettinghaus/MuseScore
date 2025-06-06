bcf75731a67885f2173c41b478dab79457b2cb0d
rettinghaus <klaus.rettinghaus@gmail.com>
2025-06-06 13:23:07 +0100

    test

--- src/importexport/musicxml/internal/musicxml/import/importmusicxmlpass1.cpp
+++ src/importexport/musicxml/internal/musicxml/import/importmusicxmlpass1.cpp
@@ -2064,7 +2064,7 @@ void MusicXmlParserPass1::defaults()
         } else if (m_e.name() == "music-font") {
             StringList fontlist = m_e.attribute("font-family").split(u',');
             std::string musicFontString = fontlist.at(0).toStdString();
-            m_score->setEngravingFont(m_score->engravingFonts()->fontByName(musicFontString));
+            m_score->setEngravingFont(m_score->engravingFonts()->fontByName("Finale Maestro"));
             m_e.skipCurrentElement();
         } else if (m_e.name() == "word-font") {
             wordFontFamily = m_e.attribute("font-family");
