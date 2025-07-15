c4eaae451cadd9262fdbabbb1010e7a5a6c8ecd0
rettinghaus <klaus.rettinghaus@gmail.com>
2025-07-15 23:01:12 +0200

    write mscore type

--- src/importexport/mei/internal/meiexporter.cpp
+++ src/importexport/mei/internal/meiexporter.cpp
@@ -1047,9 +1047,7 @@ bool MeiExporter::writeArtics(const Chord* chord)
             }
         } else if (item->isChordLine()) {
             const ChordLine* chordline = toChordLine(item);
-            if (chordline) {
-                this->writeArtic(chordline);
-            }
+            this->writeArtic(chordline);
         }
     }
  
